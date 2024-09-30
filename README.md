9c27e151beaea120077543f8ab634866becba6e1
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2024-09-29 23:54:54 -0700

    daysto calculates the number of days from today to a specified date or between two dates.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # daysto
+
+`daysto` calculates the number of days from today to a specified date or between two dates.
+
+## Installation
+
+```
+cabal install daysto
+```
+
+## Usage
+
+```
+daysto [from] to
+
+OPTIONS
+    from    Optional start date. If omitted, today's date is used.
+    to      End date.
+
+Both dates must be in YYYY-MM-DD format.
+```
+
+## Examples
+
+```
+daysto 2038-01-19
+```
+
+```
+daysto 2021-01-01 2024-05-19
+```
--- daysto/Main.hs
+++ daysto/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Data.Time.Calendar ( Day, diffDays )
+import Data.Time.Clock ( getCurrentTime )
+import Data.Time.LocalTime ( LocalTime(localDay), utcToLocalTime, getCurrentTimeZone )
+import Data.Time.Format ( defaultTimeLocale, parseTimeM )
+import System.Environment ( getArgs )
+
+getCurrentLocalTime :: IO LocalTime
+getCurrentLocalTime = utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime
+
+parseDate :: String -> Maybe Day
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+
+daysTo :: Maybe Day -> Maybe Day -> String
+daysTo (Just to) (Just from) = show (diffDays to from)
+daysTo _ _ = ""
+
+exec :: Day -> [String] -> String
+exec today args = case args of
+    []        -> ""
+    [to]      -> daysTo (parseDate to) (Just today)
+    from:to:_ -> daysTo (parseDate to) (parseDate from)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    today <- getCurrentLocalTime
+    getArgs >>= putStrLn . exec (localDay today)
